cbf91969c22de33021dfd875ebb2f3056f905e82
Digitz <digitzetre@gmail.com>
2025-05-10 13:13:27 -0700

    refactor: remove duplicate empty component files and organize components into proper subdirectories

--- lib/components/card_visual_component.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flame/components.dart';
-import 'package:flame/game.dart';
-import 'package:flame/palette.dart';
-import 'package:card_combat_app/models/game_card.dart';
-import 'package:card_combat_app/game/card_combat_game.dart';
-import 'package:card_combat_app/utils/game_logger.dart'; 
