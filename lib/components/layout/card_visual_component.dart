96383c80935581c56fca6ff75847647ecae24626
Digitz <digitzetre@gmail.com>
2025-05-10 13:14:55 -0700

    fix: update tap event handlers to use correct Flame 1.28.1 event types

--- lib/components/layout/card_visual_component.dart
+++ lib/components/layout/card_visual_component.dart
@@ -1,6 +1,7 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/palette.dart';
+import 'package:flame/events.dart';
 import 'package:flutter/material.dart' hide Card;
 import 'package:card_combat_app/models/game_card.dart';
 import 'package:card_combat_app/game/card_combat_game.dart';
