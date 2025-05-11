e3ed8dc5bbf42853f56af0a188994ff272a95aee
Digitz <digitzetre@gmail.com>
2025-05-11 02:23:45 -0700

    refactor: move player UI to dedicated PlayerPanel component

--- lib/components/layout/combat_scene_layout.dart
+++ lib/components/layout/combat_scene_layout.dart
@@ -3,20 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:card_combat_app/models/player/player_base.dart';
 import 'package:card_combat_app/models/enemies/enemy_base.dart';
 import 'package:card_combat_app/models/game_card.dart';
-import 'package:card_combat_app/components/layout/card_visual_component.dart';
-import 'package:card_combat_app/components/layout/cards_panel.dart';
+import 'package:card_combat_app/components/layout/player_panel.dart';
 import 'package:card_combat_app/managers/combat_manager.dart';
 import 'package:card_combat_app/utils/game_logger.dart';
 
 class CombatSceneLayout extends Component {
   final Vector2 size;
-  final CardsPanel cardsPanel;
+  final PlayerPanel playerPanel;
   final TextComponent turnText;
   final TextComponent enemyNextActionText;
-  final TextComponent playerHealthText;
   final TextComponent enemyHealthText;
   final TextComponent gameMessageText;
-  final List<CardVisualComponent> cardComponents = [];
 
   late CombatManager combatManager;
 
@@ -26,13 +23,14 @@ class CombatSceneLayout extends Component {
     required EnemyBase enemy,
     required Function(GameCard) onCardPlayed,
   }) : 
-    cardsPanel = CardsPanel(
-      position: Vector2(size.x / 2, size.y - 100),
-      size: Vector2(size.x, 200),
+    playerPanel = PlayerPanel(
+      size: size,
+      player: player,
+      onCardPlayed: onCardPlayed,
     ),
     turnText = TextComponent(
       text: "Player's Turn",
-      position: Vector2(size.x / 2, 100),
+      position: Vector2(size.x * 0.5, size.y * 0.1),
       textRenderer: TextPaint(
         style: const TextStyle(
           color: Colors.white,
@@ -44,7 +42,7 @@ class CombatSceneLayout extends Component {
     ),
     enemyNextActionText = TextComponent(
       text: 'Next: ',
-      position: Vector2(size.x / 2, 140),
+      position: Vector2(size.x * 0.5, size.y * 0.15),
       textRenderer: TextPaint(
         style: const TextStyle(
           color: Colors.white,
@@ -53,19 +51,9 @@ class CombatSceneLayout extends Component {
       ),
       anchor: Anchor.topCenter,
     ),
-    playerHealthText = TextComponent(
-      text: '',
-      position: Vector2(50, 50),
-      textRenderer: TextPaint(
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-        ),
-      ),
-    ),
     enemyHealthText = TextComponent(
       text: '',
-      position: Vector2(size.x - 50, 50),
+      position: Vector2(size.x * 0.8, size.y * 0.05),
       textRenderer: TextPaint(
         style: const TextStyle(
           color: Colors.white,
@@ -76,7 +64,7 @@ class CombatSceneLayout extends Component {
     ),
     gameMessageText = TextComponent(
       text: '',
-      position: Vector2(size.x / 2, size.y / 2),
+      position: Vector2(size.x * 0.5, size.y * 0.5),
       textRenderer: TextPaint(
         style: const TextStyle(
           color: Colors.white,
@@ -89,18 +77,16 @@ class CombatSceneLayout extends Component {
 
   void initialize(PlayerBase player, EnemyBase enemy, CombatManager combatManager) {
     this.combatManager = combatManager;
+    playerPanel.initialize(player, combatManager);
     updateUI(combatManager);
   }
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    
-    // Add components to the scene
-    add(cardsPanel);
+    add(playerPanel);
     add(turnText);
     add(enemyNextActionText);
-    add(playerHealthText);
     add(enemyHealthText);
   }
 
@@ -111,47 +97,11 @@ class CombatSceneLayout extends Component {
     // Update enemy's next action
     enemyNextActionText.text = 'Next: ${combatManager.enemy.getNextAction()}';
     
-    // Update health displays
-    playerHealthText.text = '${combatManager.player.name}: ${combatManager.player.currentHealth}/${combatManager.player.maxHealth}';
+    // Update enemy health display
     enemyHealthText.text = '${combatManager.enemy.name}: ${combatManager.enemy.currentHealth}/${combatManager.enemy.maxHealth}';
 
-    // Update cards
-    updateCards(combatManager.playerHand, combatManager.isPlayerTurn);
-  }
-
-  void updateCards(List<GameCard> cards, bool isPlayerTurn) {
-    // Remove old cards
-    for (var card in cardComponents) {
-      cardsPanel.remove(card);
-    }
-    cardComponents.clear();
-
-    // Add new cards
-    for (var i = 0; i < cards.length; i++) {
-      _addCardToHand(cards[i], i, isPlayerTurn);
-    }
-  }
-
-  void _addCardToHand(GameCard card, int index, bool isPlayerTurn) {
-    final cardWidth = CardsPanel.cardWidth;
-    final cardHeight = CardsPanel.cardHeight;
-    
-    GameLogger.info(LogCategory.game, 'Adding card to hand: ${card.name}');
-    
-    // Calculate position using the CardsPanel's helper method
-    final position = cardsPanel.calculateCardPosition(index);
-    
-    final cardComponent = CardVisualComponent(
-      card,
-      position: position,
-      size: Vector2(cardWidth, cardHeight),
-      onCardPlayed: (card) => combatManager.playCard(card),
-      enabled: isPlayerTurn,
-    );
-    
-    // Add card to the cards panel
-    cardsPanel.add(cardComponent);
-    cardComponents.add(cardComponent);
+    // Update player panel
+    playerPanel.updateUI(combatManager);
   }
 
   void showGameMessage(String message) {
--- lib/components/layout/player_panel.dart
+++ lib/components/layout/player_panel.dart
@@ -1,12 +1,92 @@
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-import 'base_panel.dart';
+import 'package:card_combat_app/models/player/player_base.dart';
+import 'package:card_combat_app/models/game_card.dart';
+import 'package:card_combat_app/components/layout/card_visual_component.dart';
+import 'package:card_combat_app/components/layout/cards_panel.dart';
+import 'package:card_combat_app/managers/combat_manager.dart';
+import 'package:card_combat_app/utils/game_logger.dart';
 
-class PlayerPanel extends BasePanel {
-  PlayerPanel(Vector2 gameSize) : super(
-    gameSize: gameSize,
-    characterEmoji: '🧙',
-    hpColor: Colors.white,
-    isTop: false,
-  );
+class PlayerPanel extends Component {
+  final Vector2 size;
+  final CardsPanel cardsPanel;
+  final TextComponent playerHealthText;
+  final List<CardVisualComponent> cardComponents = [];
+
+  late CombatManager combatManager;
+
+  PlayerPanel({
+    required this.size,
+    required PlayerBase player,
+    required Function(GameCard) onCardPlayed,
+  }) : 
+    cardsPanel = CardsPanel(
+      position: Vector2(0, size.y * 0.75),
+      size: Vector2(size.x * 0.8, size.y * 0.2),
+    ),
+    playerHealthText = TextComponent(
+      text: '',
+      position: Vector2(size.x * 0.2, size.y * 0.05),
+      textRenderer: TextPaint(
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+        ),
+      ),
+    );
+
+  void initialize(PlayerBase player, CombatManager combatManager) {
+    this.combatManager = combatManager;
+    updateUI(combatManager);
+  }
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    add(cardsPanel);
+    add(playerHealthText);
+  }
+
+  void updateUI(CombatManager combatManager) {
+    // Update health display
+    playerHealthText.text = '${combatManager.player.name}: ${combatManager.player.currentHealth}/${combatManager.player.maxHealth}';
+
+    // Update cards
+    updateCards(combatManager.playerHand, combatManager.isPlayerTurn);
+  }
+
+  void updateCards(List<GameCard> cards, bool isPlayerTurn) {
+    // Remove old cards
+    for (var card in cardComponents) {
+      cardsPanel.remove(card);
+    }
+    cardComponents.clear();
+
+    // Add new cards
+    for (var i = 0; i < cards.length; i++) {
+      _addCardToHand(cards[i], i, isPlayerTurn);
+    }
+  }
+
+  void _addCardToHand(GameCard card, int index, bool isPlayerTurn) {
+    final cardWidth = CardsPanel.cardWidth;
+    final cardHeight = CardsPanel.cardHeight;
+    
+    GameLogger.info(LogCategory.game, 'Adding card to hand: ${card.name}');
+    
+    // Calculate position using the CardsPanel's helper method
+    final position = cardsPanel.calculateCardPosition(index);
+    
+    final cardComponent = CardVisualComponent(
+      card,
+      position: position,
+      size: Vector2(cardWidth, cardHeight),
+      onCardPlayed: (card) => combatManager.playCard(card),
+      enabled: isPlayerTurn,
+    );
+    
+    // Add card to the cards panel
+    cardsPanel.add(cardComponent);
+    cardComponents.add(cardComponent);
+  }
 } 
