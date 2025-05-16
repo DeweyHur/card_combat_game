de84ca7c4a5fdf0129dbe3cb7529188076e1ddcf
Digitz <digitzetre@gmail.com>
2025-05-16 12:57:25 -0700

    Refactor ArmorySceneLayout to use VerticalStackMixin for vertical stacking of components

--- lib/components/layout/armory_scene_layout.dart
+++ lib/components/layout/armory_scene_layout.dart
@@ -2,8 +2,9 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:card_combat_app/scenes/scene_manager.dart';
 import 'package:card_combat_app/components/panel/equipment_panel.dart';
+import 'package:card_combat_app/components/mixins/vertical_stack_mixin.dart';
 
-class ArmorySceneLayout extends PositionComponent {
+class ArmorySceneLayout extends PositionComponent with VerticalStackMixin {
   late final TextComponent _titleText;
   late final PositionComponent _backButton;
   bool _isLoaded = false;
@@ -13,6 +14,9 @@ class ArmorySceneLayout extends PositionComponent {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    resetVerticalStack();
+
+    // Title
     _titleText = TextComponent(
       text: 'Armory / Equipment',
       textRenderer: TextPaint(
@@ -22,19 +26,19 @@ class ArmorySceneLayout extends PositionComponent {
           fontWeight: FontWeight.bold,
         ),
       ),
-      anchor: Anchor.center,
+      anchor: Anchor.topCenter,
+      size: Vector2(size.x, 50),
     );
-    add(_titleText);
+    addToVerticalStack(_titleText, 50);
 
-    // Add EquipmentPanel below the title
-    final equipmentPanel = EquipmentPanel(size: Vector2(size.x, size.y * 0.28))
-      ..position = Vector2(0, size.y * 0.32)
-      ..anchor = Anchor.topLeft;
-    add(equipmentPanel);
+    // Equipment Panel
+    final equipmentPanel = EquipmentPanel(size: Vector2(size.x, size.y * 0.28));
+    addToVerticalStack(equipmentPanel, size.y * 0.28);
 
+    // Back Button
     _backButton = PositionComponent(
       size: Vector2(160, 48),
-      anchor: Anchor.center,
+      anchor: Anchor.topCenter,
     )
       ..add(RectangleComponent(
         size: Vector2(160, 48),
@@ -55,23 +59,16 @@ class ArmorySceneLayout extends PositionComponent {
           position: Vector2(80, 24),
         ),
       );
-    add(_backButton);
+    addToVerticalStack(_backButton, 60);
 
     _isLoaded = true;
-    _updatePositions(size);
   }
 
   @override
   void onGameResize(Vector2 size) {
     super.onGameResize(size);
-    if (_isLoaded) {
-      _updatePositions(size);
-    }
-  }
-
-  void _updatePositions(Vector2 size) {
-    _titleText.position = Vector2(size.x / 2, size.y * 0.25);
-    _backButton.position = Vector2(size.x / 2, size.y * 0.8);
+    this.size = size;
+    // Optionally, you could re-layout children here if dynamic resizing is needed
   }
 
   void handleTap(Vector2 pos) {
